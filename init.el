978d52854e41be8a160d2e5b154151d2345aea22
Joe Mitchell Jones <joe.mitchelljones@couchbase.com>
2022-09-08 14:14:45 +0100

    Python and Go improvements, couple of evil tweaks

--- init.el
+++ init.el
@@ -23,32 +23,30 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
- '(compilation-message-face (quote default))
+ '(compilation-message-face 'default)
  '(custom-safe-themes
-   (quote
-    ("c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" "e4859645a914c748b966a1fe53244ff9e043e00f21c5989c4a664d649838f6a3" default)))
+   '("c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" "e4859645a914c748b966a1fe53244ff9e043e00f21c5989c4a664d649838f6a3" default))
  '(fci-rule-color "#3C3D37")
- '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
+ '(flycheck-checker-error-threshold nil)
+ '(helm-minibuffer-history-key "M-p")
+ '(highlight-changes-colors '("#FD5FF0" "#AE81FF"))
  '(highlight-tail-colors
-   (quote
-    (("#3C3D37" . 0)
+   '(("#3C3D37" . 0)
      ("#679A01" . 20)
      ("#4BBEAE" . 30)
      ("#1DB4D0" . 50)
      ("#9A8F21" . 60)
      ("#A75B00" . 70)
      ("#F309DF" . 85)
-     ("#3C3D37" . 100))))
+     ("#3C3D37" . 100)))
  '(magit-diff-use-overlays nil)
  '(package-selected-packages
-   (quote
-    (exec-path-from-shell key-chord org-bullets monokai-theme powerline-evil powerline ace-window golden-ratio lorem-ipsum company evil-magit magit ace-jump-mode helm which-key general ivy evil peacock-theme)))
+   '(yaml-mode terraform-mode go-mode flycheck undo-tree evil-collection exec-path-from-shell key-chord org-bullets monokai-theme powerline-evil powerline ace-window golden-ratio lorem-ipsum company evil-magit magit ace-jump-mode helm which-key general ivy evil peacock-theme))
  '(pos-tip-background-color "#FFFACE")
  '(pos-tip-foreground-color "#272822")
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
-   (quote
-    ((20 . "#F92672")
+   '((20 . "#F92672")
      (40 . "#CF4F1F")
      (60 . "#C26C0F")
      (80 . "#E6DB74")
@@ -65,11 +63,10 @@
      (300 . "#299BA6")
      (320 . "#2896B5")
      (340 . "#2790C3")
-     (360 . "#66D9EF"))))
+     (360 . "#66D9EF")))
  '(vc-annotate-very-old-color nil)
  '(weechat-color-list
-   (quote
-    (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0"))))
+   '(unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -131,7 +128,7 @@
 
 (use-package golden-ratio)
 (golden-ratio-mode 1)
-(setq golden-ratio-auto-scale t)
+;;(setq golden-ratio-auto-scale t)
 
 (use-package evil-collection
   :init (evil-collection-init))
@@ -144,6 +141,8 @@
 (use-package ace-window)
 
 (use-package magit)
+
+(use-package undo-tree)
 
 (use-package powerline)
 (use-package powerline-evil)
@@ -159,10 +158,27 @@
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
 
+(use-package go-mode)
+(add-hook 'go-mode-hook 'lsp-deferred)
+
+(use-package terraform-mode)
+
+(use-package yaml-mode)
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
 (defun find-user-init-file ()
   "Edit config in another window"
   (interactive)
   (find-file-other-window user-init-file))
+
+(defun add-py-debug ()
+      "add debug code and move line down"
+    (interactive)
+    (move-beginning-of-line 1)
+    (insert "import pdb; pdb.set_trace();\n"))
 
 (use-package general)
 ;; bind j and k in normal state globally
@@ -185,6 +201,10 @@
  :keymaps 'normal
  "mg" 'magit-status
  "mh" 'magit-dispatch-popup
+ "mb" 'magit-blame
+ "gw" 'godef-jump-other-window
+ "gj" 'godef-jump
+ "gd" 'godef-describe
  "d" 'dired
  "c" 'find-user-init-file
  "f" 'find-file
@@ -211,11 +231,13 @@
   "W" 'ace-window
   "b" 'ivy-switch-buffer
   "P" 'helm-mini
+  "i" 'helm-imenu-in-all-buffers
   "w" 'ace-jump-word-mode
   "c" 'ace-jump-char-mode
   "l" 'ace-jump-line-mode
   "L" 'lorem-ipsum-insert-paragraphs
   "B" 'eval-buffer
+  "d" 'add-py-debug
   "ol" 'org-store-link
   "oa" 'org-agenda
   "ot" 'org-todo
@@ -252,4 +274,9 @@
 (if (eq system-type 'windows-nt)
     (remove-hook 'org-mode-hook 'turn-on-flyspell)
 )
+
+(setq evil-undo-system 'undo-tree)
+(global-undo-tree-mode)
+(define-key evil-normal-state-map "u" 'undo-tree-undo)
+(define-key evil-normal-state-map (kbd "C-r") 'undo-tree-redo)
 
